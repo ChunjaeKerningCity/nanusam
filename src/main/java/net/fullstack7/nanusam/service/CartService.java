a1ff0403abc155314a187a3778b12a0082d872a5
java7 <java7@fullstack7.net>
2024-11-14 18:33:43 +0900

    wqe

--- src/main/java/net/fullstack7/nanusam/service/CartService.java
+++ src/main/java/net/fullstack7/nanusam/service/CartService.java
@@ -7,7 +7,8 @@ import net.fullstack7.nanusam.dto.PageResponseDTO;
 import java.util.List;
 
 public interface CartService {
-    public int totalCount();
+    public int totalCount(String memberId);
+    public List<CartDTO> list();
     public List<CartDTO> list(String memberId);
     public PageResponseDTO<CartDTO> listByPage(PageRequestDTO requestDTO);
     void add(CartDTO dto);
