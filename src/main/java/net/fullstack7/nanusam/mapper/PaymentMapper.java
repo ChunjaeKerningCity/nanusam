ed163838e2c6f58dd4aa6ecf5fdb0f18aaea48d3
java7 <java7@fullstack7.net>
2024-11-14 18:33:36 +0900

    payment list 202411141833

--- src/main/java/net/fullstack7/nanusam/mapper/PaymentMapper.java
+++ src/main/java/net/fullstack7/nanusam/mapper/PaymentMapper.java
@@ -1,6 +1,7 @@
 package net.fullstack7.nanusam.mapper;
 
 import net.fullstack7.nanusam.domain.PaymentVO;
+import net.fullstack7.nanusam.dto.PageRequestDTO;
 import net.fullstack7.nanusam.dto.PaymentDTO;
 
 import java.util.List;
@@ -9,5 +10,6 @@ public interface PaymentMapper {
     void regist(PaymentVO payment);
     int deleteByIdx(int idx);
 
-    List<PaymentDTO> listWithGoodsByBuyer(String buyer);
+    List<PaymentDTO> listWithGoodsByBuyer(PageRequestDTO pageRequestDTO);
+    int total_count(PageRequestDTO pageRequestDTO);
 }
--- src/main/java/net/fullstack7/nanusam/service/PaymentService.java
+++ src/main/java/net/fullstack7/nanusam/service/PaymentService.java
@@ -1,10 +1,10 @@
 package net.fullstack7.nanusam.service;
 
+import net.fullstack7.nanusam.dto.PageRequestDTO;
+import net.fullstack7.nanusam.dto.PageResponseDTO;
 import net.fullstack7.nanusam.dto.PaymentDTO;
-
-import java.util.List;
 
 public interface PaymentService {
     String regist(PaymentDTO dto);
-    List<PaymentDTO> listWithGoodsByBuyer(String buyer);
+    PageResponseDTO<PaymentDTO> listWithGoodsByBuyer(PageRequestDTO pageRequestDTO);
 }
