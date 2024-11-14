27d3bdc27e0953bc65c4aabdeda2cd5151e174e9
[kangkamchan] <[topthanks@naver.com]>
2024-11-14 18:32:05 +0900

    강감찬/0014 리뷰 하는중

--- src/main/java/net/fullstack7/nanusam/controller/ReviewController.java
+++ src/main/java/net/fullstack7/nanusam/controller/ReviewController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Log4j2
@@ -36,6 +37,7 @@ public class ReviewController {
             , Model model
             , HttpServletResponse res
             ) {
+        res.setCharacterEncoding("UTF-8");
         String errors = listValid(pageRequestDTO,memberType);
         if(errors!=null){
             JSFunc.alertBack(errors,res);
@@ -60,17 +62,32 @@ public class ReviewController {
     }
 
     @GetMapping("/regist.do")
-    public String registGet(Model model){
+    public String registGet(@RequestParam(required = false, defaultValue = "")String buyer, Model model, HttpSession session,HttpServletResponse response) {
+        if(buyer.isEmpty()){
+            JSFunc.alertBack("구매자정보가 없습니다.",response);
+            return null;
+        }
+        model.addAttribute("buyer",buyer);
         return "review/regist";
     }
 
     @PostMapping("/regist.do")
-    public String registPost(@Valid ReviewDTO reviewDTO, BindingResult bindingResult, Model model){
+    public String registPost(@Valid ReviewDTO reviewDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model, HttpSession session,HttpServletResponse response){
         if(bindingResult.hasErrors()){
-            return "review/regist";
+            StringBuilder errors = new StringBuilder();
+            bindingResult.getAllErrors().forEach(error -> errors.append(error.getDefaultMessage()).append("<br>"));
+            JSFunc.alertBack(errors.toString(),response);
+            return null;
         }
 
-        return "redirect:/review/view.do";
+        int result = reviewService.regist(reviewDTO);
+
+        if(result<=0){
+            JSFunc.alertBack("후기등록실패",response);
+            return null;
+        }
+        redirectAttributes.addFlashAttribute("errors","후기등록성공");
+        return "redirect:/review/list.do?memberType=buyer&memberId="+reviewDTO.getBuyer();
     }
 
     private String listValid(PageRequestDTO pageRequestDTO, String memberType) {
