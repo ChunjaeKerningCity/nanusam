7f344cd882253bcee13119503f8ddf691d279389
soojin <ssj1737@gmail.com>
2024-11-16 21:21:43 +0900

    필터 수정중

--- src/main/java/net/fullstack7/nanusam/filter/LoginFilter.java
+++ src/main/java/net/fullstack7/nanusam/filter/LoginFilter.java
@@ -1,5 +1,6 @@
 package net.fullstack7.nanusam.filter;
 
+import net.fullstack7.nanusam.util.CommonUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import javax.servlet.*;
@@ -11,6 +12,7 @@ import java.net.URLEncoder;
 
 @WebFilter(urlPatterns = {"/member/*", "/payment/*","/review/*","/cart/*","/chat/*","/bbs/view.do","/goods/regist.do","/goods/modify.do","/goods/delete.do"})
 public class LoginFilter  implements Filter {
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -18,25 +20,19 @@ public class LoginFilter  implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
+
         if(request.getSession().getAttribute("memberId") == null){
-            // 문제 있으면 이 부분 지우셈...
-            //String originalURL = request.getRequestURI();
-            String originalURL = request.getHeader("Referer");
+            String referer = request.getHeader("referer");
+            String refererUri = referer != null ? CommonUtil.urlToUri(referer) : "";
 
-//            if (originalURL == null || originalURL.isEmpty()) {
-//                originalURL = request.getRequestURI();
-//                if (request.getQueryString() != null) {
-//                    originalURL += "?" + request.getQueryString();
-//                }
-//            }
-
-            if (request.getQueryString() != null) {
-                originalURL += "?" + request.getQueryString();
+            if (refererUri != null && !refererUri.isEmpty()) {
+                refererUri = URLEncoder.encode(URLEncoder.encode(refererUri, "UTF-8"), "UTF-8"); // 두 번 인코딩
             }
-            request.getSession().setAttribute("redirectAfterLogin", URLEncoder.encode(URLEncoder.encode(originalURL,"UTF-8"),"UTF-8"));
+            request.getSession().setAttribute("redirectAfterLogin", refererUri);
             //-------------------------
             response.setContentType("text/html;charset=UTF-8");
             response.getWriter().println("<script>");
